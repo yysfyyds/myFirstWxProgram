8fabc901c314f43bf76a5b0dd27aef149a409d5a
夜雨声烦 <106472988+yysfyyds@users.noreply.github.com>
2023-08-04 15:02:32 +0800

    Add files via upload
    工具类

---
+++ yysf_Core/Extends/EFExtends.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yysf_Core.Extends
+{
+    public static class EFExtends//扩展whereif条件语句
+    {
+        public static IQueryable<T> WhereIf<T>(this  IQueryable<T> queryable,Expression<Func<T,bool>> expression
+            ,bool condition)
+        {
+            return condition? queryable.Where(predicate: expression ): queryable;
+        }
+        public static IQueryable<T> Paging<T>(this IQueryable<T> queryable,int skipNum,int takeNum,out int count)
+        {
+            count = queryable.Count();
+            return queryable.Skip(count: skipNum).Take(count: takeNum);
+        }
+    }
+}
